d8e2f78d9b2faada936be257027094fd92095b87
David Glasser <glasser@davidglasser.net>
2015-02-17 12:41:39 -0800

    stop using autopublish

---
+++ publish.js
@@ -0,0 +1,15 @@
+// XXX this is temporary
+if (Meteor.isClient) {
+  Meteor.subscribe('unlabeled-open', 'meteor', 'meteor');
+} else {
+  Meteor.publish('unlabeled-open', function (repoOwner, repoName) {
+    check(repoOwner, String);
+    check(repoName, String);
+    return Issues.find({
+      repoOwner: repoOwner,
+      repoName: repoName,
+      'issueDocument.open': true,
+      'issueDocument.hasProjectLabel': false
+    });
+  });
+}
