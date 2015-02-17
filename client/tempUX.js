ccbbfee00f40264236cee0b740fa2b4fe3c4f6e6
ekatek <ekate@meteor.com>
2015-02-16 18:36:46 -0800

    label colors

--- client/tempUX.js
+++ client/tempUX.js
@@ -1,5 +1,17 @@
 Template.currentIssues.helpers({
   issues: function () {
-    return Issues.find({"issueDocument.closedAt": null});
+    return Issues.find({
+        "issueDocument.closedAt": null,
+        "issueDocument.hasProjectLabel": false
+    }, { $sort: { "lastMessage.timestamp": 1 } });
+  }
+});
+
+Template.labeledIssues.helpers({
+  issues: function () {
+    return Issues.find({
+        "issueDocument.closedAt": null,
+        "issueDocument.hasProjectLabel": true
+    }, { $sort: { "lastMessage.timestamp": 1 } });
   }
 });
