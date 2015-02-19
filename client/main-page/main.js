d39bf1438cfe351053939b1352db68b029b18135
ekatek <ekate@meteor.com>
2015-02-18 17:47:06 -0800

    more UI

---
+++ client/main-page/main.js
@@ -0,0 +1,8 @@
+Template.issueNav.helpers({
+  states: function () {
+    return [
+      { name: "Triaged", color: "33aa55", selected: true },
+      { name: "Stirring", color: "FAAC58", selected: false }
+     ];
+   },
+});
