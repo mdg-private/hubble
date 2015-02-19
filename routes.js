a5f78d885a2e40754c2d21bfc84d7b3e3742d5d6
ekatek <ekate@meteor.com>
2015-02-18 22:20:37 -0800

    basic routing support
    Support for basic routing -- now you can access filters with a URL.
    
    I am not sure that it is a good idea to redirect you, whenever the URL
    shows up, rather than just letting you get the link to save a search.

---
+++ routes.js
@@ -0,0 +1,40 @@
+Router.configure({
+  layoutTemplate: "hello"
+});
+
+Router.route('/', function () {
+  this.layout("hello");
+  this.render("tempUX");
+});
+
+var setTag = function (tag) {
+  Session.set("labelFilter", tag);
+};
+
+var setStates = function (states) {
+  var selected = states.split("&");
+  _.each(selected, function (state) {
+      States.update({ tag: state }, {$set: { selected: true }});
+   });
+};
+
+Router.route('/states/:_states', function () {
+  this.layout("hello");
+  setStates(this.params._states);
+  this.render("generic");
+}, 'client');
+
+
+Router.route('/filter/:_tag', function () {
+  this.layout("hello");
+  setTag(this.params._tag);
+  this.render("generic");
+}, 'client');
+
+
+Router.route('/states/:_states/filter/:_tag', function () {
+  this.layout("hello");
+  setStates(this.params._states);
+  setTag(this.params._tag);
+  this.render("generic");
+}, 'client');
