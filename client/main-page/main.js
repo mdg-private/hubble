d2011c86a616256c82176193751171a824552b72
ekatek <ekate@meteor.com>
2015-02-18 19:40:54 -0800

    be able to select states
    You can now select states, though there is still no filtering.
    
    Also, removed a useless old template, replacing it with the better current template.

--- client/main-page/main.js
+++ client/main-page/main.js
@@ -1,8 +1,50 @@
 Template.issueNav.helpers({
   states: function () {
-    return [
-      { name: "Triaged", color: "33aa55", selected: true },
-      { name: "Stirring", color: "FAAC58", selected: false }
-     ];
+    return States.find({}, { sort: { urgency: -1 }});
    },
+  selected: function () {
+    return Session.get(selectedState(this.tag));
+  }
+});
+
+Template.viewIssues.helpers({
+  issues: function () {
+    return Issues.find({
+        "issueDocument.open": true,
+        "issueDocument.hasProjectLabel": true
+    }, { $sort: { "issueDocument.updatedAt": -1 } });
+  }
+});
+
+Template.issueNav.events({
+  'click .state-button' : function () {
+    Session.set(selectedState(this.tag), ! Session.get(selectedState(this.tag)));
+  }
+});
+
+var selectedState = function (tag) {
+  return "selectedState:" + tag;
+};
+
+States = new Mongo.Collection(null);
+Meteor.startup(function () {
+  if (! States.findOne()) {
+    // new
+    States.insert({ tag: "new", name: "Unresponded", color: "D2B91B", urgency: 10 });
+
+    // active
+    States.insert({ tag: "active", name: "Active", color: "F22", urgency: 7  });
+
+    // triaged
+    States.insert({ tag: "triaged", name: "Triaged", color: "33aa55",  urgency: 3 });
+
+    // closed
+    States.insert({ tag: "closed", name: "Closed", color: "777", urgency: 0 });
+
+    // stirring
+    States.insert({ tag: "stirring", name: "Stirring", color: "FAAC58", urgency: 9 });
+
+    // highly-active
+    States.insert({ tag: "highly-active", name: "Highly-Active", color: "77F",  urgency: 5 });
+  }
 });
