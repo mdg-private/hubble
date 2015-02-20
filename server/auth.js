457b2d9371ba026d0069532936b992b51d514140
David Glasser <glasser@davidglasser.net>
2015-02-20 12:17:10 -0800

    unbreak avatars

--- server/auth.js
+++ server/auth.js
@@ -1,6 +1,10 @@
 // An extra layer of protection, especially since we trust GitHub's HTML.
 BrowserPolicy.content.disallowInlineScripts();
 
+// ... but we should allow GitHub avatar images.
+BrowserPolicy.content.allowImageOrigin(
+  'https://avatars.githubusercontent.com/');
+
 Accounts.validateLoginAttempt(function (info) {
   check(info.user, Match.ObjectIncluding({
     services: Match.ObjectIncluding({
