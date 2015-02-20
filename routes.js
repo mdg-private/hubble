93fafe28f7c81044229e78556a9b1b7a15128326
ekatek <ekate@meteor.com>
2015-02-20 13:17:04 -0800

    moving some functions around for cleaner code

--- routes.js
+++ routes.js
@@ -48,3 +48,18 @@ Router.route('/unlabeled', function () {
   this.layout('hello');
   this.render('unlabeled');
 }, 'client');
+
+
+compileLink = function () {
+  var url = "";
+  if (States.findOne({ selected: true })) {
+    var selected = States.find({ selected: true }).fetch();
+    var statesStr = _.pluck(selected, "tag").join("+");
+    url += "/states/" + statesStr;
+  }
+  var filter = Session.get('labelFilter');
+  if (filter) {
+    url += "/filter/" + filter.join("+");
+  }
+  return url;
+};
