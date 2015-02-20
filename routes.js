7cc2be2824c3689a48f54380d5f05f37709197a0
David Glasser <glasser@davidglasser.net>
2015-02-19 17:09:14 -0800

    add xxx comments for things that confuse me

--- routes.js
+++ routes.js
@@ -14,13 +14,16 @@ var setTag = function (tag) {
 var setStates = function (states) {
   var selected = states.split("&");
   _.each(selected, function (state) {
-      States.update({ tag: state }, {$set: { selected: true }});
-   });
+    States.update({ tag: state }, {$set: { selected: true }});
+  });
+  // XXX glasser doesn't understand why this doesn't have to deselect the other
+  // states too
 };
 
 Router.route('/states/:_states', function () {
   this.layout("hello");
   setStates(this.params._states);
+  // XXX glasser doesn't understand why this doesn't have to unset filter too
   this.render("generic");
 }, 'client');
 
@@ -28,6 +31,7 @@ Router.route('/states/:_states', function () {
 Router.route('/filter/:_tag', function () {
   this.layout("hello");
   setTag(this.params._tag);
+  // XXX glasser doesn't understand why this doesn't have to unset states too
   this.render("generic");
 }, 'client');
 
